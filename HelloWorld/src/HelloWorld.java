2656bd278bb7f690e0ceff2a9453af6fa6677f0e
Yusuf Savaş <yusufsss4@gmail.com>
2021-05-03 12:43:52 +0300

    Employee Example is Added

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,8 +3,25 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world!");
-		System.out.println("AnothetTest!");
+		Employee empOne = new Employee("James Smith");
+		Employee empTwo = new Employee("Yusuf Savas");
+		
+		empOne.empAge(26);
+		empOne.empDesignation("Software Engineer");
+		empOne.empSalary(1000);
+		empOne.printEmployee();
+		empTwo.empAge(21);
+		empTwo.empDesignation("Junior");
+		empTwo.empSalary(1000);
+		empTwo.printEmployee();
+
 	}
 
 }
+
+class FreshJuice {
+	enum FreshJuiceSize {SMALL,MEDIUM,LARGE}
+	FreshJuiceSize size;
+}
+
+
