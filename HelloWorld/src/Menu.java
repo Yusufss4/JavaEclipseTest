df238fa8ae0165d5c8b7fcb101f3d5323f3a3f8b
Efe Özel <Efe zel@DESKTOP-LARO49K>
2021-05-09 21:37:48 +0300

    wedding and normal menus

--- HelloWorld/src/Menu.java
+++ HelloWorld/src/Menu.java
@@ -48,7 +48,7 @@ class ConcertMenu implements Menu {
 			System.out.println("MenuStyle: " + menuStyle);
 			System.out.println("Appetizer: " + (appetizer == null ? "Appetizer is not selected" : appetizer));
 			System.out.println("Beverage: " + (beverage == null ? "Beverage is not selected" : beverage));
-			System.out.println("Sandwich: " + (beverage == null ? "Sandwich is not selected" : sandwich));
+			System.out.println("Sandwich: " + (sandwich == null ? "Sandwich is not selected" : sandwich));
 		}
 
 	}
@@ -75,17 +75,57 @@ class WeddingMenu implements Menu {
 	private String mainCourse;
 	private String side;
 	private String dessert;
+	private String menuStyle;
+	private String[] menuStyles = { "Menu1", "Menu2" };
 
-	public void addItemsToTheMenu(String appetizer) {
+
+	public void addItemsToTheMenu(String menuStyle) {
+		this.menuStyle = menuStyle;
+		switch (menuStyle) {
+		case "Menu1":
+			this.appetizer = "Food1";
+			this.beverage = "Food1";
+			this.mainCourse = "Food1";
+			this.side = "Food1";
+			this.dessert = "Food1";
+			break;
+		case "Menu2":
+			this.appetizer = "Food2";
+			this.beverage = "Food2";
+			this.mainCourse = "Food2";
+			this.side = "Food2";
+			this.dessert = "Food2";
+			break;
+		default:
+			System.out.println("\nMenu style cant be empty.");
+		}
+		
 	}
 
 	public void getMenu() {
+		if (menuStyle == null) {
+			System.out.println("Menu is not selected.");
+		} else {
+			System.out.println("-- Menu Details --");
+			System.out.println("MenuStyle: " + menuStyle);
+			System.out.println("Appetizer: " + (appetizer == null ? "Appetizer is not selected" : appetizer));
+			System.out.println("Beverage: " + (beverage == null ? "Beverage is not selected" : beverage));
+			System.out.println("mainCourse: " + (mainCourse == null ? "Main Course is not selected" : mainCourse));
+			System.out.println("side: " + (side == null ? "Side is not selected" : side));
+			System.out.println("dessert: " + (dessert == null ? "Dessert is not selected" : dessert));
+		}
 	}
 
 	public void changeMenuStyle(String menuStyle) {
+		this.menuStyle = menuStyle;
+		addItemsToTheMenu(menuStyle);
 	}
 
 	public void getAvailableMenuStyles() {
+		for (String i : menuStyles) {
+			System.out.println(i);
+		}
+		
 	}
 
 	public void changePreMadeMenuStyles() {
@@ -102,15 +142,53 @@ class NormalMenu implements Menu {
 	private String[] menuStyles = { "Menu1", "Menu2" };
 
 	public void addItemsToTheMenu(String menuStyle) {
+		this.menuStyle = menuStyle;
+		switch (menuStyle) {
+		case "Menu1":
+			this.appetizer = "Food1";
+			this.beverage = "Food1";
+			this.mainCourse = "Food1";
+			this.side = "Food1";
+			this.dessert = "Food1";
+			break;
+		case "Menu2":
+			this.appetizer = "Food2";
+			this.beverage = "Food2";
+			this.mainCourse = "Food2";
+			this.side = "Food2";
+			this.dessert = "Food2";
+			break;
+		default:
+			System.out.println("\nMenu style cant be empty.");
+		}
 	}
 
 	public void getMenu() {
+		if (menuStyle == null) {
+			System.out.println("Menu is not selected.");
+		} else {
+			System.out.println("-- Menu Details --");
+			System.out.println("MenuStyle: " + menuStyle);
+			System.out.println("Appetizer: " + (appetizer == null ? "Appetizer is not selected" : appetizer));
+			System.out.println("Beverage: " + (beverage == null ? "Beverage is not selected" : beverage));
+			System.out.println("mainCourse: " + (mainCourse == null ? "Main Course is not selected" : mainCourse));
+			System.out.println("side: " + (side == null ? "Side is not selected" : side));
+			System.out.println("dessert: " + (dessert == null ? "Dessert is not selected" : dessert));
+		}
+		
 	}
+	
+	
 
 	public void changeMenuStyle(String menuStyle) {
+		this.menuStyle = menuStyle;
+		addItemsToTheMenu(menuStyle);
 	}
 
 	public void getAvailableMenuStyles() {
+		for (String i : menuStyles) {
+			System.out.println(i);
+		}
 	}
 
 	public void changePreMadeMenuStyles() {
